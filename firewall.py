602c66dc9b2ddb18ad82a63f059c0537e13d70d0
sascha <sastrand@pdx.edu>
2018-10-18 06:40:21 -0700

    expanded, updated firewall_eg.py

--- firewall.py
+++ firewall.py
@@ -4,19 +4,18 @@
 # Please see the file LICENSE for the source
 # distribution of this software for license terms
 
-from sys import argv
-from ipaddress import ip_address
 from time import time
 
-# takes a file pointer to file of ip address 
+
+# takes a file pointer to file of ip address
 # and returns a list of those ip addresses as Ints
 def ip_reader(fp):
     buf = fp.readlines()
     ret = []
     for ip in buf:
-        ip = int(ip_address(ip.rstrip()))
         ret.append(ip)
     return ret
+
 
 # Node class of a BST
 # Create a root node for use with insert and search with
@@ -24,39 +23,56 @@ def ip_reader(fp):
 class BST(object):
     def __init__(self, val):
         self.val = val
-        self.l = None
-        self.r = None
+        self.left = None
+        self.right = None
 
     def __str__(self):
-        return '({}, {}, {})'.format(self.l, self.val, self.r)
+        return '({}, {}, {})'.format(self.left, self.val, self.right)
 
-def insert(val, parent):
+    def insert(val, parent):
     pass
 
-def search(val, parent):            
+    def search(val, parent):
     pass
 
+if __name__ == __main__:
+
+with open("./data/traffic_test.txt", 'r') as f:
+    traf = ip_reader(f)
+
+with open("./data/whitelist_test.txt", 'r') as f:
+    wl = ip_reader(f)
 
 # Code block to time list
-# var wl is the whitelist
-# var traf is the list of all intercepted IPs
 t0 = time()
 ret = 0
 for ip in traf:
     if ip in wl:
-        ret+=1
+        ret += 1
 t1 = time()
- 
-print("IPs matches in list:{}. Time: {} seconds".format(ret, t1-t0))
+
+print("IPs matches in list:{}. Time: {} seconds".format(ret, t1 - t0))
 
 # Code block to time BST
-# var root is the root node of the BST
+root = BST(wl[0])
+[insert(x, root) for x in wl]
+
 t2 = time()
 ret = 0
 for ip in traf:
     if search(ip, root):
-        ret+=1
+        ret += 1
 t3 = time()
 
-print("IPs matched in tree:{}. Time: {} seconds".format(ret, t3-t2))
+print("IPs matched in tree:{}. Time: {} seconds".format(ret, t3 - t2))
 
+# Code block to time set
+wl_set = set(wl)
+t4 = time()
+ret = 0
+for ip in traf:
+    if ip in wl_set:
+        ret += 1
+t5 = time()
+
+print("IPs matched in set:{}. Time: {} seconds".format(ret, t5 - t4))
--- firewall_eg.py
+++ firewall_eg.py
@@ -4,77 +4,88 @@
 # Please see the file LICENSE for the source
 # distribution of this software for license terms
 
-from sys import argv
-from ipaddress import ip_address
 from time import time
+
 
 class BST(object):
     def __init__(self, val):
         self.val = val
-        self.l = None
-        self.r = None
+        self.left = None
+        self.right = None
 
     def __str__(self):
         return '({}, {}, {})'.format(self.l, self.val, self.r)
 
-def insert(val, parent):
-    if val == parent.val: return
-    if val < parent.val:
-        if parent.l:
-            insert(val, parent.l)
+    def insert(self, val_in):
+        if val_in == self.val: return
+        if val_in < self.val:
+            if self.left:
+                self.left.insert(val_in)
+            else:
+                self.left = BST(val_in)
         else:
-            parent.l = BST(val)
-    else:
-        if parent.r:
-            insert(val, parent.r)
-        else:
-            parent.r = BST(val)
+            if self.right:
+                self.right.insert(val_in)
+            else:
+                self.right = BST(val_in)
 
-def search(val, parent):
-    if val == parent.val: return True
-    if val < parent.val:
-        if parent.l:
-            return search(val, parent.l)
+    def search(self, val_in):
+        if val_in == self.val: return True
+        if val_in < self.val:
+            if self.left:
+                return self.left.search(val_in)
+            else:
+                return None
         else:
-            return None
-    else:
-        if parent.r:
-            return search(val, parent.r)
-        else:
-            return None
+            if self.right:
+                return self.right.search(val_in)
+            else:
+                return None
+
 
 def ip_reader(fp):
     buf = fp.readlines()
     ret = []
     for ip in buf:
-        ip = int(ip_address(ip.rstrip()))
         ret.append(ip)
     return ret
 
-with open(argv[1], 'r') as f:
+
+with open("./data/traffic.txt", 'r') as f:
     traf = ip_reader(f)
 
-with open(argv[2], 'r') as f:
+with open("./data/whitelist.txt", 'r') as f:
     wl = ip_reader(f)
 
 t0 = time()
 ret = 0
 for ip in traf:
     if ip in wl:
-        ret+=1
+        ret += 1
 t1 = time()
- 
-print("IPs matches in list:{}. Time: {} seconds".format(ret, t1-t0))
 
-root = BST(wl.pop())
-[insert(x, root) for x in wl]
+print("IPs matches in list: {}. Time: {} seconds".format(ret, t1 - t0))
+
+root = BST(wl[0])
+[root.insert(x) for x in wl]
 
 t2 = time()
 ret = 0
 for ip in traf:
-    if search(ip, root):
-        ret+=1
+    if root.search(ip):
+        ret += 1
 t3 = time()
 
-print("IPs matched in tree:{}. Time: {} seconds".format(ret, t3-t2))
+print("IPs matched in tree: {}. Time: {} seconds".format(ret, t3 - t2))
 
+# Code block to time set
+# var wl_set is the whitelist as a Python set
+wl_set = set(wl)
+t4 = time()
+ret = 0
+for ip in traf:
+    if ip in wl_set:
+        ret += 1
+t5 = time()
+
+print("IPs matched in set:  {}. Time: {} seconds".format(ret, t5 - t4))
